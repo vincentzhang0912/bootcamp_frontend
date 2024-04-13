9471342e6af110e21dc7f4ab5112226ca53bdc2f
vincent <vincentzys0912@gmail.com>
2024-04-12 19:50:40 -0700

    useeffect

---
+++ day29/useeffect/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import UseEffectCheck from './components/MyUseEffectCheck';
+
+function App() {
+  return (
+    <div className="App">
+      <UseEffectCheck/>
+    </div>
+  );
+}
+
+export default App;
---
+++ day29/useeffect/src/components/MyUseEffectCheck.js
@@ -0,0 +1,40 @@
+import {useEffect, useState} from 'react'
+const UseEffectCheck = () => {
+  let [counter,setCounter]=useState(0);
+  let [str,setStr]=useState("--");
+  //useEffect is used as componentWillMount in class Component
+
+  //this is called before mounting/rendering the component
+  //will be called twice if index.js has strict mode on.
+  //componnetWillUpdate,componentDidUpdate
+
+  //gets called every time component re-renders
+  //componentWillUpdate,componentDidUpdate
+  useEffect(()=>{
+    console.log('From use effect');
+    //used to we need to talk to remote server  to get details and store it in state
+    //we write code that will be called when ever component is re-render
+    document.title = counter + str;
+  });
+
+  let clickHandler=()=>{
+    counter++;
+    setCounter(counter);
+  }
+  let clickHandlerStr=()=>{
+    str +="**"
+    setStr(str)
+  }
+  return ( 
+    <div>
+      <h1>UseEffectCheck</h1>
+      <h3>{counter}</h3>
+      <h3>{str}</h3>
+      <h3></h3>
+      <button onClick={clickHandler}>increment</button>
+      <button onClick={clickHandlerStr}>add**</button>
+    </div>
+   );
+}
+ 
+export default UseEffectCheck;
